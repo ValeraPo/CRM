8d2663752e2a4d3205431f912c45fa5d4c428800
Valeriia <portnovavalerija@mail.ru>
2022-03-02 21:33:06 +0300

    added entities

---
+++ CRM_DataLayer/Entities/Lead.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_DataLayer.Entities
+{
+    internal class Lead
+    {
+    }
+}
