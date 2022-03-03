1730f8f0e1aaf056c6c2a5ebbf23311ed4883bfb
ll <jokzik@gmail.com>
2022-03-03 14:52:29 +0300

    start work

---
+++ CRM_DataLayer/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace CRM_DataLayer
+{
+    public class Account
+    {   
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public string CurrencyType { get; set; }
+    }
+}
