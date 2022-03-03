873f1878d86084a7318a816d593e9005daf68416
ll <jokzik@gmail.com>
2022-03-03 15:36:10 +0300

    start to create procedure

---
+++ CRM_DB/Tables/Lead.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Lead]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Name] varchar(20) NOT NULL,
+	[LastName] varchar(20) NOT NULL,
+	[DateBirth] date NOT NULL,
+	[Email] varchar(30) NOT NULL,
+	[Phone] varchar(20) NOT NULL,
+	[Password] varchar(150) NOT NULL,
+	CONSTRAINT AK_Email UNIQUE(Email),
+	CONSTRAINT AK_Phone UNIQUE(Phone)
+)
--- CRM_DataLayer/Lead.cs
+++ CRM_DataLayer/Lead.cs
@@ -11,8 +11,9 @@ namespace CRM_DataLayer
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
-        public string Phone { get; set; }
+        public DateTime DateBirth { get; set; }
         public string Email { get; set; }
+        public string Phone { get; set; }
         public string Password { get; set; }
         public List<Account> Accounts { get; set; }
         public Enum Role { get; set; }
