258738d04b6eb66451c4926cc996f34905ee3360
Valeriia <portnovavalerija@mail.ru>
2022-03-04 21:53:20 +0300

    validation

--- CRM/Models/Input/LeadInputModel.cs
+++ CRM/Models/Input/LeadInputModel.cs
@@ -4,8 +4,13 @@ namespace CRM.APILayer.Models
 {
     public class LeadInputModel
     {
+        [StringLength(30, ErrorMessage = "Максимальная длина 30 символов.")]
         public string Name { get; set; }
+
+        [StringLength(30, ErrorMessage = "Максимальная длина 30 символов.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Поле BirthDate не может быть пустым.")]
         public DateTime BirthDate { get; set; }
 
         [Phone(ErrorMessage = "Телефон введен некорректно.")]
