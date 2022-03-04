bd03f037eed7f9f303c2f33f3b55bfc7c9fc02f2
ll <jokzik@gmail.com>
2022-03-04 16:38:09 +0300

    fix after merge

--- CRM_DataLayer/Repositories/LeadRepository.cs
+++ CRM_DataLayer/Repositories/LeadRepository.cs
@@ -1,21 +1,23 @@
 ﻿using CRM_DataLayer.Repositories.Interfaces;
+using Dapper;
 using System.Data;
 
 
 namespace CRM_DataLayer.Repositories
 {
-    public class LeadRepository : ILeadRepository
+    public class LeadRepository : BaseRepository, ILeadRepository
     {
         private const string _updateProc = "dbo.Lead_Update";
         private const string _insertProc = "dbo.Lead_Insert";
-        //провести через DL connection_string
-        public LeadRepository()
-        {
 
+        public LeadRepository(string conn) : base(conn)
+        {
         }
 
         public void AddLead(Lead lead)
         {
+            using IDbConnection connection = ProvideConnection();
+
             connection.Execute(_insertProc,
                new
                {
@@ -30,40 +32,6 @@ namespace CRM_DataLayer.Repositories
                },
 
                commandType: CommandType.StoredProcedure);
-        }
-
-        public void UpdateLeadById(Lead lead)
-        {
-            //вставить тут connection_string
-            connection.Execute(_updateProc,
-                new
-                {
-                    Id = lead.Id,
-                    Name = lead.Name,
-                    LastName = lead.LastName,
-                    DateBirth = lead.DateBirth,
-                    Email = lead.Email,
-                    Phone = lead.Phone,
-                    Role = lead.Role
-                },
-
-                commandType: CommandType.StoredProcedure);
-        }
-
-    }
-}
-
-﻿using Dapper;
-using System.Data;
-
-namespace CRM_DataLayer.Repositories
-{
-    public class LeadRepository : BaseRepository
-    {
-        private const string _updateProc = "dbo.Lead_Update";
-
-        public LeadRepository(string conn) : base(conn)
-        {
         }
 
         public void UpdateLeadById(Lead lead)
@@ -87,3 +55,5 @@ namespace CRM_DataLayer.Repositories
 
     }
 }
+
+﻿
