351e397a026c2aef3dd9e4ae736d8cd5aee029b2
ll <jokzik@gmail.com>
2022-03-22 23:21:49 +0300

    fix

---
+++ CRM.BusinessLayer.Tests/ServiceTests/TransactionServiceTests.cs
@@ -0,0 +1,15 @@
+﻿
+using Moq;
+using NUnit.Framework;
+
+namespace CRM.BusinessLayer.Tests.ServiceTests
+{
+    public class TransactionServiceTests
+    {
+
+        public TransactionServiceTests()
+        {
+
+        }
+    }
+}
